eb01a155b9954eb3589d70acc219d182835ab3b6
Amiruddin Irsyad <amiruddin.irsyad@ifg-life.id>
2023-03-17 21:00:29 +0700

    upload file on disk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const Book = require("./book");
+const upload = require("./middleware/upload");
 const { PORT = 8000 } = process.env;
 
 //Book.js tambahkan module.exports = Book
@@ -8,7 +9,9 @@ const { PORT = 8000 } = process.env;
 //const lastRecordId =
 // this.constructor.records[this.constructor.records.length - 1]
 
+app.use(express.static("public"))
 app.use(express.json())
+
 
 //list
 app.get('/api/v1/books', (req, res) => {
@@ -31,10 +34,34 @@ app.post('/api/v1/books', (req, res) => {
         data: books
     })
 })
-// //update / edit
-// app.put('/api/v1/books/:id',)
-// //delete
-// app.delete('/api/v1/books/', )
+// update / edit
+app.put('/api/v1/books/:id', (req, res) => {
+    const book = Book.find(req.params.id)
+    if(!book) return res.status(404).json({
+        error: "Book Not Found"
+    })
+
+    book.update(req.body)
+    res.status(200).json(book);
+})
+// delete
+app.delete('/api/v1/books/:id', (req, res) => {
+    const book = Book.find(req.params.id)
+    if(!book) return res.status(404).json({
+        error: "Book Not Found"
+    })
+
+    book.delete()
+    res.status(204).end();
+})
+// upload
+app.post('/api/v1/upload/', upload.single("picture"), (req, res) => {
+    const url = `/uploads/${req.file.filename}`;
+    res
+      .status(200)
+      .json({ message: "Foto berhasil di-upload, silahkan cek URL", url });
+
+})
 
 app.use((req, res) => {
     res.status(404).send("Mau kemana bos?")
