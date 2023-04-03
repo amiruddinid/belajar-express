5ea1453a1b758b18558fd6532ab6fbc3201aaac1
Amiruddin Irsyad <amiruddin.irsyad@ifg-life.id>
2023-04-03 22:34:27 +0700

    Authorize with token

--- app/controllers/api/v1/auth.js
+++ app/controllers/api/v1/auth.js
@@ -72,4 +72,23 @@ module.exports = {
       });
     }
   },
+
+  async whoami(req, res){
+    res.status(200).json(req.user)
+  },
+
+  async authorize(req, res, next){
+    try{
+        const bearerToken = req.headers.authorization;
+        const token = bearerToken.split("Bearer ")[1];
+        const tokenPayload = jwt.verify(token, process.env.JWT_SIGNATURE_KEY || 'Rahasia')
+
+        req.user = await User.findByPk(tokenPayload.id);
+        next();
+    } catch(e) {
+        res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+  }
 };
--- config/routes.js
+++ config/routes.js
@@ -7,13 +7,14 @@ const apiRouter = express.Router();
 appRouter.get("/", controllers.main.index);
 
 apiRouter.get("/api/v1/motors", controllers.api.v1.motor.list);
-apiRouter.post("/api/v1/motors", controllers.api.v1.motor.create);
+apiRouter.post("/api/v1/motors", controllers.api.v1.auth.authorize, controllers.api.v1.motor.create);
 apiRouter.put("/api/v1/motors/:id", controllers.api.v1.motor.setMotor, controllers.api.v1.motor.update);
 apiRouter.get("/api/v1/motors/:id", controllers.api.v1.motor.setMotor, controllers.api.v1.motor.show);
 apiRouter.delete("/api/v1/motors/:id",controllers.api.v1.motor.setMotor, controllers.api.v1.motor.destroy);
 
 apiRouter.post("/api/v1/auth/register", controllers.api.v1.auth.register);
 apiRouter.post("/api/v1/auth/login", controllers.api.v1.auth.login);
+apiRouter.get("/api/v1/auth/whoami", controllers.api.v1.auth.authorize, controllers.api.v1.auth.whoami);
 
 apiRouter.use(controllers.main.onLost);
 apiRouter.use(controllers.api.main.onError);
